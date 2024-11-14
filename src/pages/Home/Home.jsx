edc987f177b587cf3c0ca4d879327eb4188c147b
Semharit <isayyas88@gmail.com>
2024-11-14 15:51:58 -0500

    first commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Header from '../../components/Header/Header.jsx'
+ import Footer from '../../components/Footer/Footer.jsx'
+ import Banner from '../../components/Banner/Banner.jsx'
+import RowList from '../../components/Rows/RowList/RowList.jsx'
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowList />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
